b06a5ea1bc9d1f834b7bc474109a1f7b571a06f3
unknown <aarthiv2k@gmail.com>
2021-04-26 08:24:34 +0530

    update

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ io.on("connection", socket => {
 });
 
 
-server.listen(8000, () => console.log('server is running on port 8000'));
+server.listen(process.env.PORT||8000, () => console.log('server is running on port 8000',this.address().port, app.settings.env));
